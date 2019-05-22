5efd0c17d94426c1ce724bfb3aa758ffc665b09f
Paul Lockwood <paul.lockwood@proliant.com>
2019-05-22 08:15:17 -0400

    Tested as working

--- src/app/customers/customers.module.ts
+++ src/app/customers/customers.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { CustomersRoutingModule } from './customers-routing.module';
 import { CustomerListComponent } from './customer-list/customer-list.component';
 
 @NgModule({
-  declarations: [CustomerListComponent],
   imports: [
     CommonModule,
     CustomersRoutingModule
-  ]
+  ],
+  declarations: [CustomerListComponent]
 })
 export class CustomersModule { }
